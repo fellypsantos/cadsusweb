fdb0fc0d1ec0d8d7ed4a0cdb49e818336ad1b4bd
Fellyp Santos <fellypsantos2011@gmail.com>
2020-03-11 13:34:01 -0300

    Bundle gerado com todas as depencencias, até dev.

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
-const package = require('./package.json');
 
 module.exports = {
   mode: 'production',
@@ -10,9 +8,4 @@ module.exports = {
     filename: 'cns.bundle.js'
   },
   target: 'node',
-  externals: [
-    nodeExternals({
-      whitelist: Object.keys(package.dependencies)
-    })
-  ]
 };
