99b0e816b8c3b4d8cbfba3b703606a5703497b63
fellypsantos <fellypsantos2011@gmail.com>
2024-01-17 15:07:35 -0300

    refactor: pass mongodb ip via command line

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,22 @@
 import express, { json } from 'express';
 import cors from 'cors';
-
 import routes from './routes';
+
+import './dateFnsInit';
 import './database/model/User';
+
 import { initTemplateEngine } from './views/nunjucks';
 import { dbConnect } from './database';
 
+
+const mongodbParam = process.argv.indexOf('--mongodb');
+const mongodbServer = mongodbParam > -1 ? process.argv[mongodbParam + 1] : 'localhost';
+
 const api = express();
 
+
 initTemplateEngine(api);
-dbConnect();
+dbConnect(mongodbServer);
 
 api.set('view engine', 'njk');
 api.use(json());
