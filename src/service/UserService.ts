26f51a469d8b080d6712ac6d7638b077d7a9ee4b
fellypsantos <fellypsantos2011@gmail.com>
2024-01-17 16:32:07 -0300

    refactor: logic to add user to db

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,16 +1,42 @@
 import chalk from 'chalk';
+import notifier from 'node-notifier';
 import User, { UserDocument } from '../database/model/User';
 import Logger from './Logger';
+import { UserType } from '../types/UserType';
 
 export const findUserByCns = async (numeroCns: string): Promise<UserDocument | null> => {
   const user = await User.findOne({ numeroCns });
 
   if (!user) return null;
 
+  return user;
+};
+
+export const handleAddUser = async (userdata: UserType): Promise<UserDocument | null> => {
+  const { numeroCns } = userdata;
+  const user = await findUserByCns(numeroCns);
+
+  if (user) {
+    Logger(
+      chalk.bgYellow(numeroCns),
+      chalk.yellow('Cadastro ja existe no banco de dados offline.')
+    );
+    return null;
+  }
+
+  const createdUser = await User.create(userdata);
+
   Logger(
-    chalk.bgGreen(`${numeroCns}`),
-    chalk.green('Recuperado da base local.')
+    chalk.bgBlue(createdUser.numeroCns),
+    chalk.blue(createdUser.nome),
+    'Salvo offline.'
   );
 
-  return user;
+  notifier.notify({
+    title: 'CADSUS Local',
+    message: 'Usuário foi salvo offline.',
+    sound: false
+  });
+
+  return createdUser;
 };
