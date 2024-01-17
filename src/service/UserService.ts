29becf75c9e5b7f7edeb87e057be90c5ebeae209
fellypsantos <fellypsantos2011@gmail.com>
2024-01-17 12:48:03 -0300

    style: print bgGreen when find card locally

--- src/service/UserService.ts
+++ src/service/UserService.ts
@@ -1,11 +1,12 @@
-import User, { UserDocument } from '../model/User';
+import chalk from 'chalk';
+import User, { UserDocument } from '../database/model/User';
 
 export const findUserByCns = async (numeroCns: string): Promise<UserDocument | null> => {
   const user = await User.findOne({ numeroCns });
 
   if (!user) return null;
 
-  console.log(`[${numeroCns}] Recuperado da base local.`);
+  console.log(chalk.bgGreen(`${numeroCns}`), chalk.green('Recuperado da base local.'));
 
   return user;
 };
