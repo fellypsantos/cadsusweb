3624d46d228703c35bbcc67daebec6e25895f965
fellypsantos <fellypsantos2011@gmail.com>
2024-01-17 17:19:49 -0300

    chore: add routes to update and delete user

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,7 @@
 import { Router } from 'express';
-import { addUser, findUser } from './controller/UserController';
+import { addUser, findUser, updateUser } from './controller/UserController';
 import { generateCards } from './controller/CardController';
+import { handleDeleteUser } from './service/UserService';
 
 const routes = Router();
 
@@ -11,5 +12,7 @@ routes.get('/', (_, response) => {
 routes.get('/find_user/:cns', findUser);
 routes.get('/card/:cns', generateCards);
 routes.post('/user', addUser);
+routes.put('/user', updateUser);
+routes.delete('/user/:id', handleDeleteUser);
 
 export default routes;
