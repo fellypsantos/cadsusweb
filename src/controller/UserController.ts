29949f977d4be248ae22cf97728e25b6a822b97a
fellypsantos <fellypsantos2011@gmail.com>
2024-01-17 12:47:27 -0300

    refactor: renamed function

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import { findUserByCns } from '../service/UserService';
 
-export const userExists = async (request: Request, response: Response): Promise<Response> => {
+export const findUser = async (request: Request, response: Response): Promise<Response> => {
   const { cns } = request.params;
 
   const user = await findUserByCns(cns);
