cce200ea80b434a0b3371cc210979e2c8873e399
fellypsantos <fellypsantos2011@gmail.com>
2024-01-17 23:42:32 -0300

    fix: path for dev and prod envs

--- src/controller/UserscriptController.ts
+++ src/controller/UserscriptController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import path from 'path';
 
 export const getUserScriptContent = (_: Request, response: Response): void => {
-  const scriptPath = path.join(__dirname, '..', 'tampermonkey', 'cadsusweb.user.js');
+  const rootDir = process.env.NODE_ENV === 'production' ? process.cwd() : path.join(process.cwd(), 'src');
+  const scriptPath = path.join(rootDir, 'tampermonkey', 'cadsusweb.user.js');
   response.sendFile(scriptPath);
 };
--- src/views/nunjucks.ts
+++ src/views/nunjucks.ts
@@ -3,7 +3,8 @@ import { configure } from 'nunjucks';
 import path from 'path';
 
 export const initTemplateEngine = (express: Express): void => {
-  const viewsPath = path.join(__dirname, 'views');
+  const rootDir = process.env.NODE_ENV === 'production' ? process.cwd() : path.join(process.cwd(), 'src');
+  const viewsPath = path.join(rootDir, 'views');
 
   configure(viewsPath, {
     autoescape: true,
