6333463107543b1c6dc5ca97740c46e86ea7972e
fellypsantos <fellypsantos2011@gmail.com>
2024-01-17 22:19:55 -0300

    fix: nunjucks views path

--- src/views/nunjucks.ts
+++ src/views/nunjucks.ts
@@ -1,8 +1,11 @@
 import { Express } from 'express';
 import { configure } from 'nunjucks';
+import path from 'path';
 
 export const initTemplateEngine = (express: Express): void => {
-  configure('./src/views', {
+  const viewsPath = path.join(__dirname, 'views');
+
+  configure(viewsPath, {
     autoescape: true,
     express: express,
     watch: true
